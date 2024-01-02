faba3896c10cbd4e02e8967e82ef5f061a75448b
niyioo <niyiolagbegi@gmail.com>
2024-01-02 04:53:55 -0800

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the given number of rows (n).
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1] + [prev_row[j - 1] + prev_row[j] for j in range(1, i)] + [1]
+        triangle.append(new_row)
+
+    return triangle
