692e2335e5e077ed53051845249ff9e9b83ef5ca
niyioo <niyiolagbegi@gmail.com>
2024-01-30 10:04:40 +0100

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,7 @@ UTF-8 Validation
 """
 
 
-def validUTF8(data):
+def validUTF8(data) -> bool:
     """
     Determines if a given data set represents a valid UTF-8 encoding.
 
@@ -14,22 +14,25 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding, else False.
     """
-    # Iterate through the data bytes
-    i = 0
-    while i < len(data):
-        # Count the number of set high bits in the starting byte
-        high_bits = bin(data[i])[2:].zfill(8).index('0')
+    remaining_bytes = 0
 
-        # Check if the number of following bytes matches the expected count
-        if high_bits == 1 or high_bits > 4 or i + high_bits - 1 >= len(data):
-            return False
+    for byte in data:
+        leading_bit_mask = 1 << 7
 
-        # Check that the following bytes all start with '10'
-        for j in range(1, high_bits):
-            if not (data[i + j] >> 6 == 0b10):
+        if not remaining_bytes:
+            while byte & leading_bit_mask:
+                remaining_bytes += 1
+                leading_bit_mask >>= 1
+
+            if not remaining_bytes:
+                continue
+
+            if remaining_bytes == 1 or remaining_bytes > 4:
+                return False
+        else:
+            if byte >> 6 != 0b10:
                 return False
 
-        # Move the index to the next character
-        i += high_bits
+        remaining_bytes -= 1
 
-    return True
+    return remaining_bytes == 0
