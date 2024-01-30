3c96ae58e13cd3a7741771686567a19796c8b7cd
niyioo <niyiolagbegi@gmail.com>
2024-01-30 09:55:07 +0100

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,31 +14,22 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding, else False.
     """
-    # Helper function to check if the given byte is a valid starting byte
-    def is_starting_byte(byte):
-        return (byte >> 6) != 0b10
-
-    following_bytes = [0, 1, 1, 1, 1, 2, 2, 3, 3]
-
     # Iterate through the data bytes
     i = 0
     while i < len(data):
         # Count the number of set high bits in the starting byte
-        high_bits = bin(data[i] >> 3)[2:].count('1')
+        high_bits = bin(data[i])[2:].zfill(8).index('0')
 
         # Check if the number of following bytes matches the expected count
-        if high_bits > 0:
-            if high_bits > 4 or i + following_bytes[high_bits] > len(data):
+        if high_bits == 1 or high_bits > 4 or i + high_bits - 1 >= len(data):
+            return False
+
+        # Check that the following bytes all start with '10'
+        for j in range(1, high_bits):
+            if not (data[i + j] >> 6 == 0b10):
                 return False
 
-            # Check that the following bytes all start with '10'
-            for j in range(1, following_bytes[high_bits]):
-                if not (data[i + j] >> 6 == 0b10):
-                    return False
-
-            # Move the index to the next character
-            i += following_bytes[high_bits]
-        else:
-            i += 1
+        # Move the index to the next character
+        i += high_bits
 
     return True
