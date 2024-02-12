275889208abb96fe900c66801eee4920eda96cf7
niyioo <niyiolagbegi@gmail.com>
2024-02-12 05:42:18 -0800

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,23 +6,23 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 request(apiUrl, (error, response, body) => {
-    if (error) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
         console.error('Error:', error);
         return;
-    }
+      }
 
-    const movieData = JSON.parse(body);
-    const characters = movieData.characters;
-
-    characters.forEach(characterUrl => {
-        request(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error('Error:', error);
-                return;
-            }
-
-            const characterData = JSON.parse(body);
-            console.log(characterData.name);
-        });
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
     });
+  });
 });
