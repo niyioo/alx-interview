4153f38642374a84e73e26f335e10990ce6e534e
niyioo <niyiolagbegi@gmail.com>
2024-02-13 12:35:55 +0100

    v

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,20 @@
 const request = require('request');
 
 const endpoint = 'https://swapi-api.hbtn.io/api';
-const movieId = process.argv[2];
-const apiUrl = `${endpoint}/films/${movieId}/`;
+const filmId = process.argv[2];
 
-request(apiUrl, async function (error, response, body) {
-  if(error) return console.log(error);
+request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
+  if (error) return console.log(error);
 
-  let movieData = JSON.parse(body);
-  let characters = movieData.characters;
+  let characters = JSON.parse(body).characters;
 
   for (const character of characters) {
     await new Promise((resolve, reject) => {
-      request(characterUrl, (error, response, body) => {
+      request(character, (error, response, body) => {
         if (error) {
           reject(error);
         } else {
-          console.log(movieData.name);
+          console.log(JSON.parse(body).name);
           resolve(body);
         }
       });
